b48c6cb5e9226677c0f51f00b0c425804cc0d3eb
qiurongcan <105793485+qiurongcan@users.noreply.github.com>
2025-06-12 11:27:06 +0800

    add svo_to_mavros file

---
+++ my_vins/vins_to_mavros.py
@@ -0,0 +1,43 @@
+import rospy
+from geometry_msgs.msg import PoseStamped, Point
+from nav_msgs.msg import Odometry
+import math
+from pyquaternion import Quaternion
+import tf
+import sys
+
+# vehicle_type = sys.argv[1]
+# vehicle_id = sys.argv[2]
+local_pose = PoseStamped()
+local_pose.header.frame_id = 'world'
+quaternion = tf.transformations.quaternion_from_euler(0, -math.pi/2, math.pi/2)
+q = Quaternion([quaternion[3],quaternion[0],quaternion[1],quaternion[2]])
+
+def vins_callback(data):    
+    local_pose.pose.position.x = data.pose.pose.position.x
+    local_pose.pose.position.y = data.pose.pose.position.y
+    local_pose.pose.position.z = data.pose.pose.position.z
+    q_= Quaternion([data.pose.pose.orientation.w,data.pose.pose.orientation.x,data.pose.pose.orientation.y,data.pose.pose.orientation.z])
+    q_ = q_*q
+    local_pose.pose.orientation.w = q_[0]
+    local_pose.pose.orientationsud.x = q_[1]
+    local_pose.pose.orientation.y = q_[2]
+    local_pose.pose.orientation.z = q_[3]
+
+rospy.init_node('vins_transfer')
+rospy.Subscriber("/svo/imu_pose", Odometry, vins_callback,queue_size=1)
+position_pub = rospy.Publisher("/mavros/vision_pose/pose", PoseStamped, queue_size=1)
+rate = rospy.Rate(30) 
+
+while not rospy.is_shutdown():
+    if (local_pose.pose.position == Point()):
+        continue
+    else:
+        # print("Vins pose received")
+        local_pose.header.stamp = rospy.Time.now()
+        position_pub.publish(local_pose) 
+        rospy.loginfo(local_pose)
+    try:
+        rate.sleep()
+    except:
+        continue
